f06b7e917d6fb7d7daa6cd5c53be775616007138
pol1969 <podberezsky1969@gmail.com>
2019-08-17 07:51:12 +0300

    Конец 30 главы стр 557

--- bboard/main/urls.py
+++ bboard/main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import index
+from .views import index, other_page
 
 app_name = 'main'
 urlpatterns = [
+        path('<str:page>/', other_page, name='other'),
         path('', index, name='index'),
         ]
--- bboard/main/views.py
+++ bboard/main/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.template import TemplateDoesNotExist
+from django.template.loader import get_template
 
 #Create your views here.
 
 def index(request):
     return render(request, 'main/index.html')
+
+def other_page(request, page):
+    try:
+        template = get_template('main/' + page + '.html')
+    except TemplateDoesNotExist:
+        raise Http404
+    return HttpResponse(template.render(request=request))
 
 
 
