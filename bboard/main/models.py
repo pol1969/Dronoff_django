4a85b5279e6257635a4c215954a7a5c3938bd970
pol1969 <podberezsky1969@gmail.com>
2019-08-18 17:03:58 +0300

    Создан расширенный пользователь

--- bboard/main/models.py
+++ bboard/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class AdvUser(AbstractUser):
+    is_activated = models.BooleanField(default=True, db_index=True, verbose_name='Прошел активацию?')
+    send_messages = models.BooleanField(default=True, verbose_name='Слать оповещения о новых комментариях?')
+
+    class Meta(AbstractUser.Meta):
+        pass
+ 
