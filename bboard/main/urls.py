a6fec283a82137aa58247ad1aca740c3c6341489
pol1969 <podberezsky1969@gmail.com>
2019-08-19 23:50:45 +0300

    Веб-страница пользовательского профиля

--- bboard/main/urls.py
+++ bboard/main/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import index, other_page, BBLoginView
+from .views import index, other_page, BBLoginView, profile
 
 app_name = 'main'
 urlpatterns = [
         path('<str:page>/', other_page, name='other'),
         path('', index, name='index'),
         path('accounts/login/',BBLoginView.as_view(), name='login'),
+        path('accounts/pofile/',profile, name='profile'),
         ]
--- bboard/main/views.py
+++ bboard/main/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, Http404
 from django.template import TemplateDoesNotExist
 from django.template.loader import get_template
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
 
 #Create your views here.
 
@@ -19,3 +20,6 @@ def other_page(request, page):
 class BBLoginView(LoginView):
     template_name = 'main/login.html'
 
+@login_required 
+def profile(request):
+    return render(request, 'main/profile.html')
