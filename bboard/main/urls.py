727c0c9e65c8f45c11f926e9e504926dc8463bc6
pol1969 <podberezsky1969@gmail.com>
2019-08-30 07:15:43 +0300

    Реализована функция удаления пользователя стр 579

--- bboard/main/urls.py
+++ bboard/main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import index, other_page, BBLoginView, BBLogoutView,profile,ChangeUserInfoView, BBPasswordChangeView, RegisterUserView,RegisterDoneView, user_activate
+from .views import index, other_page, BBLoginView, BBLogoutView,profile,ChangeUserInfoView, BBPasswordChangeView, RegisterUserView,RegisterDoneView, user_activate, DeleteUserView
 
 app_name = 'main'
 urlpatterns = [
@@ -10,6 +10,7 @@ urlpatterns = [
         path('accounts/register/done/',RegisterDoneView.as_view(),name='register_done'),
         path('accounts/register/', RegisterUserView.as_view(),name='register'),
         path('accounts/login/',BBLoginView.as_view(), name='login'),
+        path('accounts/profile/delete/',DeleteUserView.as_view(), name='profile_delete'),
         path('accounts/logout/',BBLogoutView.as_view(), name='logout'),
         path('accounts/profile/change/',ChangeUserInfoView.as_view(), name='profile_change'),
         path('accounts/profile/',profile, name='profile'),
--- bboard/main/views.py
+++ bboard/main/views.py
@@ -5,10 +5,12 @@ from django.template.loader import get_template
 from django.contrib.auth.views import LoginView,LogoutView, PasswordChangeView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
-from django.views.generic.edit import UpdateView,CreateView
+from django.views.generic.edit import UpdateView,CreateView,DeleteView
 from django.views.generic.base import TemplateView
 from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
 from django.urls import reverse_lazy
 from django.shortcuts import get_object_or_404
 
@@ -86,4 +88,23 @@ def user_activate(request, sign):
         user.is_activated = True
         user.save()
     return render(request, template)
-    
+   
+class DeleteUserView(LoginRequiredMixin, DeleteView):
+    model = AdvUser
+    template_name = 'main/delete_user.html'
+    success_url = reverse_lazy('main:index')
+
+    def dispatch(self, request, *args,**kwargs):
+        self.user_id = request.user.pk
+        return super().dispatch(request,*args,**kwargs)
+
+    def post(self, request,*args, **kwargs):
+        logout(request)
+        messages.add_message(request,messages.SUCCESS, 'Пользователь удален')
+        return super().post(request, *args, **kwargs)
+
+    def get_object(self, queryset=None):
+        if not queryset:
+            queryset = self.get_queryset()
+        return get_object_or_404(queryset,pk=self.user_id)
+
